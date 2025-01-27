3a5306146f29d9d38098c6d50c311831be89c003
theotrin <theopinheiro12@gmail.com>
2025-01-27 18:35:18 -0400

    feat: first unit test made

---
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -0,0 +1,20 @@
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemTest
+    {
+        [Fact]
+        public void TestandoOfertaValida()
+        {
+            Rota rota = new Rota("Origem Teste","Destino Teste");
+            Periodo periodo = new Periodo(new DateTime(2024,2,1), new DateTime(2024, 2, 10));
+            double preco = 100.0;
+            var validacao = true;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Equal(validacao, oferta.EhValido);
+        }
+    }
+}
