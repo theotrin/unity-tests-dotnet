6f8ffc36efef2eaa7571f1876ea36895ea2150f1
Theo Pinheiro <19900@bemol.local>
2025-01-28 10:05:46 -0400

    feat: made `TestandoOfertaComRotaNula`

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -7,14 +7,30 @@ namespace JornadaMilhas.Test
         [Fact]
         public void TestandoOfertaValida()
         {
+            //cenário - arrange
             Rota rota = new Rota("Origem Teste","Destino Teste");
             Periodo periodo = new Periodo(new DateTime(2024,2,1), new DateTime(2024, 2, 10));
             double preco = 100.0;
             var validacao = true;
 
+            //ação - action
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+            //validação - assert
             Assert.Equal(validacao, oferta.EhValido);
+        }
+
+         [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024,2,1), new DateTime(2024, 2, 10));
+            double preco = 100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
         }
     }
 }
